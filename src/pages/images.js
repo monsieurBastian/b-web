d4cf441e12b36425569dab3eb1562db1f70970b6
Basti <bastian.gurtner@gmail.com>
2022-07-13 11:38:01 +0200

    add images and featured images to blog posts

--- src/pages/images.js
+++ src/pages/images.js
@@ -1,29 +1,50 @@
 import React from 'react'
 import { graphql } from 'gatsby'
+import { StaticImage, GatsbyImage, getImage } from 'gatsby-plugin-image'
 
 import galaxyInSpace from '../images/javier-miranda-6Pou6SeJ6cY-unsplash.jpg'
 
 const Images = ({ data }) => {
+
+  const image = getImage( data.file )
   return (
     <div>
-      <h1>Ways of adding images</h1>
+      <h1>Adding images</h1>
 
-      <h2>basic way by just importing the image from the folder</h2>
-      <img src={ galaxyInSpace } alt="3D rendered galaxy" style={{ maxWidth: 800 }}/>
-
-      <h2 class={{ paddingTop: 100 }}>with graphql</h2>
-      <img src={ data.file.publicURL } alt="a planet" style={{ maxWidth: 800 }} />
+      <img 
+        src={ galaxyInSpace } 
+        alt="3D rendered galaxy" 
+        style={{ maxWidth: 800 }}
+      />
       
+      <h2 class={{ paddingTop: 100 }}>with Gatsby Plugin Image</h2>
+      <StaticImage
+        src="../images/javier-miranda-6Pou6SeJ6cY-unsplash.jpg"
+        alt="a galaxy"
+        placeholder="blurred"
+      />
+      <GatsbyImage
+        image={ image }
+        alt="a galaxy"
+      />
+
       <p style={{ marginTop: 200 }}>Photos by <a href="https://unsplash.com/@nuvaproductions">Javier Miranda</a> on <a href="https://unsplash.com">Unsplash</a></p>
       
     </div>
   )
 }
 
-export const imagesQuery = graphql`
-  query ImagesQuery {
-    file(relativePath: {eq: "javier-miranda-b_fob3Te8Uk-unsplash.jpg"}) {
-      publicURL
+
+export const imageQuery = graphql`
+  query ImageQuery {
+    file(relativePath: {eq: "javier-miranda-OiiVv1iiB0A-unsplash.jpg"}) {
+      childImageSharp {
+        gatsbyImageData(
+          width: 600
+          placeholder: BLURRED
+          formats: [AUTO, WEBP, AVIF]
+        )
+      }
     }
   }
 `
--- src/templates/Markdown.js
+++ src/templates/Markdown.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { graphql } from 'gatsby'
+import Img from 'gatsby-image'
 
 const Markdown = ({ data }) => {
   const { markdownRemark } = data
@@ -7,6 +8,10 @@ const Markdown = ({ data }) => {
   return (
     <div>
       <h1>{ markdownRemark.frontmatter.title }</h1>
+      <Img 
+        fluid={ markdownRemark.frontmatter.featuredImage.childImageSharp.fluid } 
+        alt="" 
+      />
       <p>{ markdownRemark.frontmatter.description }</p>
       <p>put whatever you want</p>
       <div dangerouslySetInnerHTML={{ __html: markdownRemark.html }} />
@@ -22,6 +27,13 @@ export const pageQuery = graphql`
         description
         slug
         title
+        featuredImage {
+          childImageSharp {
+            fluid(maxWidth: 800) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
       }
     }
   }
