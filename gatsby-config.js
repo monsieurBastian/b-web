23a0c7edc9bef3885bd0c2acfeb6387f5d98e389
Basti <bastian.gurtner@gmail.com>
2022-07-22 11:16:56 +0200

    new icons

--- gatsby-config.js
+++ gatsby-config.js
@@ -122,5 +122,12 @@ module.exports = {
         ],
       },
     },
+    {
+      resolve: `gatsby-source-drupal`,
+      options: {
+        baseUrl: `https://www.b-web.ch/`,
+        apiBase: `jsonapi` // or api
+      }
+    }
   ]
 }
